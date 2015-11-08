27252a75644ec25cd55eeab27419d3b4a599eba7
Dan Susskind <daniel.b.susskind@gmail.com>
2015-11-08 16:12:41 +0000

    Took out inner err function

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function(err, data) {
-    if (err) { console.log(err); }
-    var dataString = data.toString();
-    response.send(dataString);
-  })
+  fs.readFileSync('index.html');
+  var dataString = data.toString();
+  response.send(dataString);
 })
 
 app.listen(app.get('port'), function() {
