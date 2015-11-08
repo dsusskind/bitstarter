c0088c61c51f1f630c68b42e23bc86a77c17e12b
Dan Susskind <daniel.b.susskind@gmail.com>
2015-11-08 15:40:23 +0000

    index.js reads from index.html

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express')
+var fs = require('fs')
+
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
+  fs.readFileSync('index.html', function(err, data) {
+    if (err) throw err;
+    var dataString = data.toString();
+    response.send(dataString)
+}})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
