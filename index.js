58290bbdc6eba70e474b456d7e256d5682c0c55f
Dan Susskind <daniel.b.susskind@gmail.com>
2015-11-08 15:59:51 +0000

    Fixed braces and parens

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   fs.readFileSync('index.html', function(err, data) {
-    if (err) throw err;
+    if (err) { console.log(err); }
     var dataString = data.toString();
-    response.send(dataString)
-}})
+    response.send(dataString);
+  })
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
